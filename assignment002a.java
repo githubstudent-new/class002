486acff315d09a6aa7396e3c911317f858376733
GitHub Sample Teacher <training+githubtrainer@github.com>
2013-07-23 13:31:21 -0700

    Create assignment002a.java
    start of fib sequence homework

---
+++ assignment002a.java
@@ -0,0 +1,7 @@
+public class Assignmenta {
+  public static void main (String args[]) {
+  //write an application that takes in numbers 
+  // from the command line, then spits out the fibinoci sequence
+  
+  }
+}
